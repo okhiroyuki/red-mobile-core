4bd79bdf301e7601b1aeef7fae180ed0be18eb5e
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-03-13 19:57:10 +0900

    add biome and remove eslint (#74)
    * feat: add biome and remove eslint
    
    * ci: update nodejs.yml

--- websocket/compass.js
+++ websocket/compass.js
@@ -4,33 +4,33 @@ const wss = new WebSocket.Server({ noServer: true });
 let ws;
 
 wss.on("connection", (w) => {
-  console.log("serial connection");
-  ws = w;
+	console.log("serial connection");
+	ws = w;
 });
 
 wss.on("message", (message) => {
-  console.log("WebSocketServer: message : %s", message);
+	console.log("WebSocketServer: message : %s", message);
 });
 
 wss.on("error", (e) => {
-  console.log("WebSocketServer: error : %s", e);
+	console.log("WebSocketServer: error : %s", e);
 });
 
 wss.on("close", () => {
-  console.log("WebSocketServer: closed");
+	console.log("WebSocketServer: closed");
 });
 
 function send(message) {
-  if (ws) {
-    ws.send(JSON.stringify(message));
-  }
+	if (ws) {
+		ws.send(JSON.stringify(message));
+	}
 }
 
 function getWss() {
-  return wss;
+	return wss;
 }
 
 module.exports = {
-  send,
-  getWss,
+	send,
+	getWss,
 };
--- websocket/init.js
+++ websocket/init.js
@@ -1,6 +1,6 @@
-const url = require("url");
+const url = require("node:url");
 
-const http = require("http");
+const http = require("node:http");
 
 const server = http.createServer();
 
@@ -31,151 +31,151 @@ const wsFcm = fcm.getWss();
 const wsFirebaseToken = firebaseToken.getWss();
 
 server.on("upgrade", (request, socket, head) => {
-  const { pathname } = url.parse(request.url);
-  console.log(`wsServer upgrade pathname: ${pathname}`);
+	const { pathname } = url.parse(request.url);
+	console.log(`wsServer upgrade pathname: ${pathname}`);
 
-  if (pathname === "/mobile/serial") {
-    if (wsSerial) {
-      wsSerial.handleUpgrade(request, socket, head, (ws) => {
-        wsSerial.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/mobile/ble") {
-    if (wsBle) {
-      wsBle.handleUpgrade(request, socket, head, (ws) => {
-        wsBle.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/mobile/compass") {
-    if (wsCompass) {
-      wsCompass.handleUpgrade(request, socket, head, (ws) => {
-        wsCompass.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/mobile/geolocation") {
-    if (wsGeolocation) {
-      wsGeolocation.handleUpgrade(request, socket, head, (ws) => {
-        wsGeolocation.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/mobile/motion") {
-    if (wsMotion) {
-      wsMotion.handleUpgrade(request, socket, head, (ws) => {
-        wsMotion.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/mobile/gyroscope") {
-    if (wsGyroscope) {
-      wsGyroscope.handleUpgrade(request, socket, head, (ws) => {
-        wsGyroscope.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/mobile/magnetic") {
-    if (wsMagnetic) {
-      wsMagnetic.handleUpgrade(request, socket, head, (ws) => {
-        wsMagnetic.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/mobile/light") {
-    if (wsLight) {
-      wsLight.handleUpgrade(request, socket, head, (ws) => {
-        wsLight.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/mobile/proximity") {
-    if (wsPromixity) {
-      wsPromixity.handleUpgrade(request, socket, head, (ws) => {
-        wsPromixity.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/mobile/nfc") {
-    if (wsNFC) {
-      wsNFC.handleUpgrade(request, socket, head, (ws) => {
-        wsNFC.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/firebase/message") {
-    if (wsFcm) {
-      wsFcm.handleUpgrade(request, socket, head, (ws) => {
-        wsFcm.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else if (pathname === "/firebase/refresh") {
-    if (wsFirebaseToken) {
-      wsFirebaseToken.handleUpgrade(request, socket, head, (ws) => {
-        wsFirebaseToken.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy();
-    }
-  } else {
-    socket.destroy();
-  }
+	if (pathname === "/mobile/serial") {
+		if (wsSerial) {
+			wsSerial.handleUpgrade(request, socket, head, (ws) => {
+				wsSerial.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/mobile/ble") {
+		if (wsBle) {
+			wsBle.handleUpgrade(request, socket, head, (ws) => {
+				wsBle.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/mobile/compass") {
+		if (wsCompass) {
+			wsCompass.handleUpgrade(request, socket, head, (ws) => {
+				wsCompass.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/mobile/geolocation") {
+		if (wsGeolocation) {
+			wsGeolocation.handleUpgrade(request, socket, head, (ws) => {
+				wsGeolocation.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/mobile/motion") {
+		if (wsMotion) {
+			wsMotion.handleUpgrade(request, socket, head, (ws) => {
+				wsMotion.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/mobile/gyroscope") {
+		if (wsGyroscope) {
+			wsGyroscope.handleUpgrade(request, socket, head, (ws) => {
+				wsGyroscope.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/mobile/magnetic") {
+		if (wsMagnetic) {
+			wsMagnetic.handleUpgrade(request, socket, head, (ws) => {
+				wsMagnetic.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/mobile/light") {
+		if (wsLight) {
+			wsLight.handleUpgrade(request, socket, head, (ws) => {
+				wsLight.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/mobile/proximity") {
+		if (wsPromixity) {
+			wsPromixity.handleUpgrade(request, socket, head, (ws) => {
+				wsPromixity.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/mobile/nfc") {
+		if (wsNFC) {
+			wsNFC.handleUpgrade(request, socket, head, (ws) => {
+				wsNFC.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/firebase/message") {
+		if (wsFcm) {
+			wsFcm.handleUpgrade(request, socket, head, (ws) => {
+				wsFcm.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else if (pathname === "/firebase/refresh") {
+		if (wsFirebaseToken) {
+			wsFirebaseToken.handleUpgrade(request, socket, head, (ws) => {
+				wsFirebaseToken.emit("connection", ws, request);
+			});
+		} else {
+			socket.destroy();
+		}
+	} else {
+		socket.destroy();
+	}
 });
 
 function listen(port) {
-  console.log(`wsServer listen port: ${port}`);
-  server.listen(port);
+	console.log(`wsServer listen port: ${port}`);
+	server.listen(port);
 }
 
 module.exports = {
-  listen,
-  sendBle: (messege) => {
-    ble.send(messege);
-  },
-  sendSerial: (message) => {
-    serial.send(message);
-  },
-  sendCompass: (message) => {
-    compass.send(message);
-  },
-  sendMotion: (message) => {
-    motion.send(message);
-  },
-  sendGeolocation: (message) => {
-    geolocation.send(message);
-  },
-  sendGyroscope: (message) => {
-    gyroscope.send(message);
-  },
-  sendMagnetic: (message) => {
-    magnetic.send(message);
-  },
-  sendLight: (message) => {
-    light.send(message);
-  },
-  sendProximity: (message) => {
-    proximity.send(message);
-  },
-  sendNFC: (message) => {
-    nfc.send(message);
-  },
-  sendFCM: (message) => {
-    fcm.send(message);
-  },
-  sendFirebaseToken: (message) => {
-    firebaseToken.send(message);
-  },
+	listen,
+	sendBle: (messege) => {
+		ble.send(messege);
+	},
+	sendSerial: (message) => {
+		serial.send(message);
+	},
+	sendCompass: (message) => {
+		compass.send(message);
+	},
+	sendMotion: (message) => {
+		motion.send(message);
+	},
+	sendGeolocation: (message) => {
+		geolocation.send(message);
+	},
+	sendGyroscope: (message) => {
+		gyroscope.send(message);
+	},
+	sendMagnetic: (message) => {
+		magnetic.send(message);
+	},
+	sendLight: (message) => {
+		light.send(message);
+	},
+	sendProximity: (message) => {
+		proximity.send(message);
+	},
+	sendNFC: (message) => {
+		nfc.send(message);
+	},
+	sendFCM: (message) => {
+		fcm.send(message);
+	},
+	sendFirebaseToken: (message) => {
+		firebaseToken.send(message);
+	},
 };
